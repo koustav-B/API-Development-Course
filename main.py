ce2f335f0f9207835bf699f3234a6c0260af3c55
Koustav Biswas <bkoustav361@gmail.com>
2025-02-25 00:25:43 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
-app=FastAPI()
+
+app = FastAPI()
 
 @app.get("/")
 def get_user():
-    return {"message":"Hello Koustav"}
+    return {"message": "Hello Koustav"}
 
 @app.get("/home")
-def get_user():
-    return {"mes":"Hello Koustav"}
+def get_home():
+    return {"mes": "Hello Koustav"}
+
+@app.post("/createpost")
+def create_posts():
+    return {"message": "Post created successfully"}
