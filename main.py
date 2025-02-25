bd5a9272d196b42adf9092ace8156d73a6d1d1a5
Koustav Biswas <bkoustav361@gmail.com>
2025-02-25 19:27:16 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional  # ✅ Import Optional
+
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
-    published: bool = True  # Default value is True
+    published: bool = True# Default value is True
+    rating: Optional[int]=4
 
 @app.post("/createpost")
 def create_posts(new_post: Post):
-    print("✅ Published Status:", new_post.published)  # Prints `True` or `False`
+    print(new_post.dict())
+    print("✅ Rating Status:", new_post.rating)  # Prints `True` or `False`
     return {"data": new_post.dict()}  # Convert to dictionary before returning
