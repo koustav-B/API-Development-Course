6efca35a844e6cb31b6b570499168150d35f388c
Koustav Biswas <bkoustav361@gmail.com>
2025-02-27 11:09:00 +0530

    Initial commit

--- postgres1.sql
+++ postgres1.sql
@@ -1,3 +1,8 @@
-select* from products where id=1 or id=2 or id=3;
+/*select* from products where id=1 or id=2 or id=3;
 
-select* from products where id in(1,2,3);
+select* from products where id in(1,2,3); 
+select* from products;*/
+select* from products where name like 'mo%';
+select* from products;
+select* from products order by price;
+select* from products order by price desc;
