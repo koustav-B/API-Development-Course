af3f4f41ca49a3e943bdd6d15a633332a30b834d
Koustav Biswas <bkoustav361@gmail.com>
2025-02-27 10:34:39 +0530

    Initial commit

--- postgres.sql
+++ postgres.sql
@@ -1,4 +1,10 @@
 /*select *from products;
 select name from products;
 select id,name,price from products;
-select id as product_id from products;*/
+select id as prosssduct_id from products;
+select* from products where id=5;
+select* from products where inventory=0;
+select* from products where price>200;
+select* from products where inventory !=0;*/
+select* from products where inventory>0 and price>20;
+select* from products where price<100 and price>20;
---
+++ postgres1.sql
@@ -0,0 +1,3 @@
+select* from products where id=1 or id=2 or id=3;
+
+select* from products where id in(1,2,3);
