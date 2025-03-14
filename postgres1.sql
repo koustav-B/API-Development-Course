37f3c2959dd4190b0c2fa1cb0d1fc943633b12e8
Koustav Biswas <bkoustav361@gmail.com>
2025-03-14 23:57:08 +0530

    Initial Commit

--- postgres1.sql
+++ postgres1.sql
@@ -1,10 +1,3 @@
-/*select* from products where id=1 or id=2 or id=3;
-
-select* from products where id in(1,2,3); 
-select* from products;*/
-select* from products where name like 'mo%';
-select* from products;
-select* from products order by price;
-select* from products order by price desc;
-select* from products limit 5;
-select* from products where price>10 limit 2;
+select* from products order by id limit 5;
+select* from products order by id limit 5 offset 2;
+insert into products(name,price,inventory) values('koustav',4,100);
